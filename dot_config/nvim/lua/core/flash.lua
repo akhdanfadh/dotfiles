fd74015cc6b73f7844e750372e52955a3c92c556
Akhdan Fadhilah <akhdan@vision.is.tohoku.ac.jp>
2024-11-06 21:03:48 +0900

    nvim: remove flash remote keybindings

--- dot_config/nvim/lua/core/flash.lua
+++ dot_config/nvim/lua/core/flash.lua
@@ -7,8 +7,8 @@ return {
     keys = {
         { "<leader><leader>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash search" },
         { "<leader>mf", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "FLASH: Treesitter Jump" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
         -- { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
 }
