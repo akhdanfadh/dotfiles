0456815bc79b8646068fbc06a61b2b2b37b69930
Akhdan Fadhilah <me@akhdan.dev>
2025-06-14 23:45:16 +0900

    nvim: remove minimal, plan to unified

--- chezmoi/dot_config/nvim-minimal/lua/improve/todo-comments.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"folke/todo-comments.nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "[T]odo" })
-		require("todo-comments").setup()
-	end,
-}
--- chezmoi/dot_config/nvim-minimal/lua/integrate/git-config.lua
+++
@@ -1,4 +0,0 @@
-return {
-	"tpope/vim-fugitive", -- Git integration
-	"tpope/vim-rhubarb", -- Fugitive-companion to interact with GitHub
-}
